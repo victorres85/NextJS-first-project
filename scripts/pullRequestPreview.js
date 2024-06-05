969551aee9aba25ba783cae24a0568cbb5e5a49e
victor-pi-almeida <victorres.almeida@gmail.com>
2024-06-05 21:48:28 +0100

    add env variables

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,4 +1,5 @@
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
+const { execSync } = require('child_process');
 
 console.log("[DEPLOY_PREVIEW]: STARTED");
 const command = "yarn deploy:staging";
