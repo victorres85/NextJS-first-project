9274e54ca2bb41c7974749710319e104ea6c6035
victor-pi-almeida <victorres.almeida@gmail.com>
2024-06-05 21:53:53 +0100

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -4,5 +4,9 @@ const { execSync } = require('child_process');
 console.log("[DEPLOY_PREVIEW]: STARTED");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: 'utf8' })
+const lines = output.split('\n');
+lines.forEach(line => console.log('line: ', line));
+const DEPLOY_PREVIEW_URL = lines[lines.length - 2];
+const DEPLOY_PROD_URL = lines[lines.length - 1];
 
 console.log(output);
