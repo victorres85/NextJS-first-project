513bf2832305b1a32542f94dae63d7e620e80806
victor-pi-almeida <victorres.almeida@gmail.com>
2024-06-08 18:09:12 +0100

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -22,27 +22,27 @@ defaultHeaders["accept"] =
   "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
 defaultHeaders["content-type"] = "application/json";
 
-console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
-console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+// console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+// console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
 
-fetch(
-  `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
-  {
-    headers: defaultHeaders,
-    method: "POST",
-    body: JSON.stringify({
-      body: GH_COMMENT,
-    }),
-  }
-)
-  .then((response) => {
-    if (response.ok) return response.json();
-    throw new Error(response.statusText);
-  })
-  .catch((err) => {
-    console.log("[COMMENT_ON_GITHUB: ERROR]");
-    throw new Error(err);
-  })
-  .finally(() => {
-    console.log("[COMMENT_ON_GITHUB: END]");
-  });
+// fetch(
+//   `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
+//   {
+//     headers: defaultHeaders,
+//     method: "POST",
+//     body: JSON.stringify({
+//       body: GH_COMMENT,
+//     }),
+//   }
+// )
+//   .then((response) => {
+//     if (response.ok) return response.json();
+//     throw new Error(response.statusText);
+//   })
+//   .catch((err) => {
+//     console.log("[COMMENT_ON_GITHUB: ERROR]");
+//     throw new Error(err);
+//   })
+//   .finally(() => {
+//     console.log("[COMMENT_ON_GITHUB: END]");
+//   });
